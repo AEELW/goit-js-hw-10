31cbe35a3fbbe0e92a7e9d8822b57836c71a7153
AEELW <130968733+AEELW@users.noreply.github.com>
2023-10-22 22:17:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import debounce from "lodash.debounce";
 import Notiflix from "notiflix";
 import "./css/styles.css";
-import { fetchCountries } from "./js/fetchCountries";
+import { fetchCountries } from 'fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
